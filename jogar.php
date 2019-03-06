96adbe73f180417442ad5de7efb017135bc0347f
Marcos Moreira <contato.marcosvmoreira@gmail.com>
2019-03-06 15:57:46 -0300

    Update: modificação de css

--- jogar.php
+++ jogar.php
@@ -33,12 +33,17 @@
             <h3>Jogar</h3>
             
 			<div class="row align-items-center">
-				<label for="pergunta" class="control-label col-md-1">Resposta:</label>
-                <input type="text" class="form-control col-md-6" name="pergunta" id="pergunta" placeholder="Insira a resposta" required autofocus/>
-                <button type="submit" class="btn btn-success">Responder</button>
-                <button class="btn btn-primary">Nova dica</button>
+				<div class="col-md-1">
+					<label for="pergunta" class="control-label">Resposta:</label>
+				</div>
+				<div class="input-group col-md-4">
+					<input type="text" class="form-control" name="pergunta" id="pergunta" placeholder="Insira a resposta" required/>
+					<button type="submit" id="btnResponder" class="btn btn-success">Responder</button>				
+				</div>
+				<div class="col-md-4">
+					<button class="btn btn-primary">Pedir nova dica</button>
+				</div>
 			</div>
-		
 		</form>
 	</div>
 </div>
